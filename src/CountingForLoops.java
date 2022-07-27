6fe7e9f44044d27b4936b9b5378991d6d4b4fab3
Stephen Acheampong <acheamse@mail.uc.edu>
2022-07-27 14:17:31 -0400

    Basic Working State after nested for loops were added!

--- src/CountingForLoops.java
+++ src/CountingForLoops.java
@@ -1,29 +1,53 @@
 public class CountingForLoops {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         // Counting loop goes from 0 to 30
-        for (int cnt = 0; cnt <= 30; cnt++)
-        {
+        for (int cnt = 0; cnt <= 30; cnt++) {
             System.out.println(cnt);
         }
 
         // Counting loop goes from 30 to 0
-        for (int cnt = 30; cnt >= 0; cnt--)
-        {
+        for (int cnt = 30; cnt >= 0; cnt--) {
             System.out.println(cnt);
         }
 
         // Counts up by three so 0,3,6,9,12,15,18
-        for (int cnt = 0; cnt <= 18; cnt = cnt + 3)
-        {
+        for (int cnt = 0; cnt <= 18; cnt = cnt + 3) {
             System.out.println(cnt);
         }
 
         // Counting loop goes down by two from 10 to 0 so 10,8,6,4,2,0
-        for (int cnt = 10; cnt >= 0; cnt = cnt - 2)
-        {
+        for (int cnt = 10; cnt >= 0; cnt = cnt - 2) {
             System.out.println(cnt);
         }
 
+
+        //NESTED FOR LOOPS
+        final int ROW = 5;
+        final int COL = 5;
+
+        //Nested for loop #1
+        for (int row = 0; row < ROW; row++) {
+            // Inner loop goes across the row through the columns
+            for (int col = 0; col <= row; col++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        //Nested for loop #2
+        for (int row = 0; row < 5; row++) {
+            for (int col = 4; col >= row; col--) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        //Nested for loop #3
+        for (int row = 0; row < 5; row++) {
+            for (int col = 0; col < 5; col ++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 }
